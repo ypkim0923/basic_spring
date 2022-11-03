98ba18750dac825e3a41722b6fc76321a08313a0
ypkim <ypkim0923@gmail.com>
2022-11-03 21:38:46 +0900

    Initial commit

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,33 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderSerivceImpl;
+import hello.core.order.OrderService;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberServiceImpl.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        
+
+        Long memberId = 1L;
+        Member member = new Member(memberId,"memberA", Grade.VIP    );
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA ", 10000 );
+
+        System.out.println("order = " + order);
+    }
+}
