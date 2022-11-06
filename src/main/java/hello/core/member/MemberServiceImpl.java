f146285973ff5e43858ad37a7b2f3323c00f65aa
ypkim <ypkim@gmail.com>
2022-11-06 14:14:55 +0900

    add component scan example

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,13 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
+    @Autowired // ac.getBean(MemberRepository.class) 와 같다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
